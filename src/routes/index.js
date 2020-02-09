9ac26192b4366d6713728d26ecb306500246ee62
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-02-08 21:40:36 -0300

    Configures private routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '~/pages/Auth/SignIn';
 import SignUp from '~/pages/Auth/SignUp';
@@ -10,7 +12,7 @@ export default function Routes() {
     <Switch>
       <Route path="/signin" component={SignIn} />
       <Route path="/signup" component={SignUp} />
-      <Route path="/" exact component={Main} />
+      <Route path="/" exact component={Main} isPrivate />
     </Switch>
   );
 }
