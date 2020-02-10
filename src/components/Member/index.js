a4c170b6296e1341f46e5145360b54810b79b9b1
Brenner L. Oliveira <brennerlimaoliveira@gmail.com>
2020-02-10 20:47:25 -0300

    Implements member listing

--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
+
+import api from '~/services/api';
 
 import Button from '~/styles/components/Button';
 import Modal from '~/components/Modal';
 import { List } from './styles';
 
 export default function Member({ close }) {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    async function loadMembers() {
+      try {
+        const response = await api.get('members');
+
+        setMembers(response.data);
+      } catch (err) {
+        toast.error(err.message);
+      }
+    }
+
+    loadMembers();
+  }, []);
+
   return (
     <Modal size="big">
       <h1>Membros</h1>
       <form>
         <List>
-          <li>
-            <strong>Brenner Oliveira</strong>
-          </li>
+          {members.map(member => (
+            <li key={member.id}>
+              <strong>{member.user.name}</strong>
+            </li>
+          ))}
         </List>
         <Button filled={false} color="gray" onClick={() => close()}>
           Cancelar
